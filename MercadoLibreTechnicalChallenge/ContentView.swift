9eca08a09cc725c28c89835e5ac0259af88645c8
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2025-04-05 23:06:27 -0500

    Initial Commit

---
+++ MercadoLibreTechnicalChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MercadoLibreTechnicalChallenge
+//
+//  Created by Juan Pablo Lasprilla Correa on 5/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MercadoLibreTechnicalChallenge/MercadoLibreTechnicalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoLibreTechnicalChallengeApp.swift
+//  MercadoLibreTechnicalChallenge
+//
+//  Created by Juan Pablo Lasprilla Correa on 5/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MercadoLibreTechnicalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
