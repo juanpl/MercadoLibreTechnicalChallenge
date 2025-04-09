7f97d86f338fc995e69009abb6551a4040c7035c
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2025-04-09 01:21:37 -0500

    Since a viewModel is going to be created for each screen and only one function needs to be injected in each viewModel, it did not make sense to inject the entire repository in each one, when it is more optimal to only inject the respective functions.

---
+++ MercadoLibreTechnicalChallenge/Features/Product/Data/UseCasesImpl/GetProductListImp.swift
@@ -0,0 +1,30 @@
+//
+//  GetProductInfoImp.swift
+//  MercadoLibreTechnicalChallenge
+//
+//  Created by Juan Pablo Lasprilla Correa on 9/04/25.
+//
+
+import Foundation
+
+class GetProductListImp: GetProductListUseCase {
+    
+    var mercadoLibreRemoteDataSource: MercadoLibreRemoteDataSource
+    
+    init(mercadoLibreRemoteDataSource: MercadoLibreRemoteDataSource) {
+        self.mercadoLibreRemoteDataSource = mercadoLibreRemoteDataSource
+    }
+    
+    
+    func getProductList(query: String, offset: Int, limit: Int) async -> Result<[ProductListItem], GetProductListError> {
+        let result = await mercadoLibreRemoteDataSource.fetchProductListFromAPI(query: query, offset: offset, limit: limit)
+
+        switch result {
+        case .success(let products):
+            return .success(products)
+        case .failure(let error):
+            print("Error en getProductList: \(error)")
+            return .failure(.getProductListFailed)
+        }
+    }
+}
